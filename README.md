dce9665c2520b19cf47399bd362159c3e60048ef
xingyun86 <xingyun_ppshuai@yeah.net>
2020-09-21 16:35:30 +0800

    提交c++20协程例子程式

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ppshuai_asiotestdemo
 ppshuai_asiotestdemo
+
+## 一个C++20协程调用程式例子，目前VS2019支持，GCC10支持。
---
+++ src/asiotestdemo/asiotestdemo/asiotestdemo.cpp
@@ -0,0 +1,14 @@
+﻿// asiotestdemo.cpp : Defines the entry point for the application.
+//
+
+#include "asiotestdemo.h"
+#include <chat_server.h>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	cout << "Hello CMake." << endl;
+	chat_server_main(argc, argv);
+	return 0;
+}
