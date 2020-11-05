b100bfbe57ec62e15fe41a65b4ea5961e8674863
xingyun86 <xingyun_ppshuai@yeah.net>
2020-11-05 11:35:14 +0800

    兼容linux下gcc10的协程编译

---
+++ src/asiotestdemo/coroutinetestdemo/coroutinetestdemo.cpp
@@ -0,0 +1,11 @@
+﻿// coroutinetestdemo.cpp : Defines the entry point for the application.
+//
+
+#include "coroutinetestdemo.h"
+#include <coroutinetestdemo_imp.h>
+int main(int argc, char ** argv)
+{
+	std::cout << "Hello CMake." << std::endl;
+	coroutine_test_main();
+	return 0;
+}
